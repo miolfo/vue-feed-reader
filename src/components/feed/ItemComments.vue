c0cdb9367e274ba6746f43fdd75f6e2a9dee9f79
miolfo <mikko.o.forsman@gmail.com>
2020-04-20 14:41:53 +0300

    Show comments recursively in single comment view

--- src/components/feed/ItemComments.vue
+++ src/components/feed/ItemComments.vue
@@ -1,14 +1,25 @@
 <template>
   <div>
-    Here is to be comments
+    <b-list-group>
+      <RecursiveCommentList :comments="item.getComments()"/>
+    </b-list-group>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Prop } from 'vue-property-decorator'
+import RecursiveCommentList from './RecursiveCommentList.vue'
 import FeedItem from '../../model/FeedItem'
+import { BListGroup } from 'bootstrap-vue'
 
-@Component
+@Component(
+  {
+    components: {
+      RecursiveCommentList,
+      BListGroup
+    }
+  }
+)
 export default class ItemComments extends Vue {
   @Prop() private item!: FeedItem;
 }
---
+++ src/components/feed/RecursiveCommentList.vue
@@ -0,0 +1,23 @@
+<template>
+  <span>
+    <b-list-group-item v-for="(comment, index) in comments" :key="index">
+      {{comment.getContent()}}
+      <RecursiveCommentList :comments="comment.getComments()"/>
+    </b-list-group-item>
+  </span>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+import Comment from '../../model/Comment'
+import { BListGroupItem } from 'bootstrap-vue'
+
+@Component({
+  components: {
+    BListGroupItem
+  }
+})
+export default class RecursiveCommentList extends Vue {
+  @Prop() private comments!: Comment[];
+}
+</script>
